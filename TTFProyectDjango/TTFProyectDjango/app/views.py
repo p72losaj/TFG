cde8abfb9b068dfecb7803ce82807fa8a471cfd3
Jaime Lorenzo Sanchez <jlorenzosa@gmail.com>
2023-04-09 20:27:15 +0200

    Inicializacion Django Web

---
+++ TTFProyectDjango/TTFProyectDjango/app/views.py
@@ -0,0 +1,17 @@
+"""
+Definition of views.
+"""
+
+from datetime import datetime
+from django.shortcuts import render
+from django.http import HttpRequest
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/plantillaBase.html'
+    )
+
+
