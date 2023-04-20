311f0d192edae1d6f197adbc1c7b094f83b1b5fa
Jaime Lorenzo Sanchez <jlorenzosa@gmail.com>
2023-04-20 12:13:36 +0200

    Plantillas paginas mineria Datos + Finalizacion paginas de tecnologias

--- TTFProyectDjango/TTFProyectDjango/TTFProyectDjango/urls.py
+++ TTFProyectDjango/TTFProyectDjango/TTFProyectDjango/urls.py
@@ -13,6 +13,16 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('tecnologias/', views.tecnologias, name='tecnologias'),
     path('tecnologias/Django/', views.dJango, name='Django'),
-    
+    path('tecnologias/html/', views.html, name='html'),
+    path('tecnologias/JavaScript/', views.JavaScript, name='JavaScript'),
+    path('tecnologias/CSS/', views.CSS, name='CSS'),
+    path('tecnologias/Python/', views.Python, name='Python'),
+    path('tecnologias/Bash/', views.Bash, name='Bash'),
+    path('MineriaDatos/', views.MineriaDatos, name='MineriaDatos'),
+    path('MineriaDatos/ObtencionDatos/', views.ObtencionDatos, name='ObtencionDatos'),
+    path('MineriaDatos/LimpiezaDatos/', views.LimpiezaDatos, name='LimpiezaDatos'),
+    path('MineriaDatos/Algoritmos/', views.AlgoritmosMiningData, name='Algoritmos'),
+    path('MineriaDatos/TransformacionDatos/', views.TransformacionDatos, name='TransformacionDatos'),
+    path('MineriaDatos/EvaluacionResultados/', views.EvaluacionResultados, name='EvaluacionResultados'),
     #path('admin/', admin.site.urls),
 ]
--- TTFProyectDjango/TTFProyectDjango/app/views.py
+++ TTFProyectDjango/TTFProyectDjango/app/views.py
@@ -23,7 +23,7 @@ def tecnologias(request):
     assert isinstance(request, HttpRequest)
     return render(
         request,
-        'app/tecnologias.html',
+        'app/tecnologias/tecnologias.html',
         {
             'title':'Tecnologias',
             'message':'Listado de tecnologias empleadas durante la realizacion del proyecto',
@@ -35,12 +35,142 @@ def dJango(request):
     assert isinstance(request, HttpRequest)
     return render(
         request,
-        'app/DJango.html',
+        'app/tecnologias/DJango.html',
         {
             'title':'Tecnologias-Django',
             'message':'Pagina de descripcion de la tecnologia Django',
         }
     )
 
+def html(request):
+    "Renders the html page"
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/tecnologias/html.html',
+        {
+            'title':'Tecnologias-HTML',
+            'message':'Pagina de descripcion de la tecnologia HTML',
+        }
+    )
 
+def JavaScript(request):
+    "Renders the JavaScript page"
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/tecnologias/JavaScript.html',
+        {
+            'title':'Tecnologias-JAVASCRIPT',
+            'message':'Pagina de descripcion de la tecnologia JavaScript',
+        }
+    )
+
+def CSS(request):
+    "Renders the CSS page"
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/tecnologias/CSS.html',
+        {
+            'title':'Tecnologias-CSS',
+            'message':'Pagina de descripcion de la tecnologia CSS',
+        }
+    )
+
+def Python(request):
+    "Renders the Python page"
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/tecnologias/Python.html',
+        {
+            'title':'Tecnologias-Python',
+            'message':'Pagina de descripcion de la tecnologia Python',
+        }
+    )
+
+def Bash(request):
+    "Renders the Bash page"
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/tecnologias/Bash.html',
+        {
+            'title':'Tecnologias-Bash',
+            'message':'Pagina de descripcion de la tecnologia Bash',
+        }
+    )
+
+def MineriaDatos(request):
+    "Renders the Mineria de datos page"
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/MineriaDatos/MineriaDatos.html',
+        {
+            'title':'Mineria de datos',
+            'message':'Pagina de descripcion de mineria de datos',
+        }
+    )
+
+def ObtencionDatos(request):
+    "Renders the MiningData/ObtencionDatos page"
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/MineriaDatos/ObtencionDatos.html',
+        {
+            'title':'Mineria de datos-Obtencion de Datos',
+            'message':'Pagina de descripcion de obtención de datos en mineria de datos',
+        }
+    )
+
+def LimpiezaDatos(request):
+    "Renders the MiningData/LimpiezaDatos page"
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/MineriaDatos/LimpiezaDatos.html',
+        {
+            'title':'Mineria de datos-Limpieza de Datos',
+            'message':'Pagina de descripcion de limpieza de datos en mineria de datos',
+        }
+    )
+
+def TransformacionDatos(request):
+    "Renders the MiningData/TransformacionDatos page"
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/MineriaDatos/TransformacionDatos.html',
+        {
+            'title':'Mineria de datos-Transformacion de Datos',
+            'message':'Pagina de descripcion de transformacion de datos en mineria de datos',
+        }
+    )
+
+def AlgoritmosMiningData(request):
+    "Renders the MiningData/Algoritmos page"
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/MineriaDatos/AlgoritmosMiningData.html',
+        {
+            'title':'Mineria de datos-Algoritmos',
+            'message':'Pagina de descripcion de algoritmos de mineria de datos',
+        }
+    )
+
+def EvaluacionResultados(request):
+    "Renders the MiningData/EvaluacionResultados page"
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/MineriaDatos/EvaluacionResultados.html',
+        {
+            'title':'Mineria de datos-Evaluacion de Resultados',
+            'message':'Pagina de descripcion de evaluacion de los resultados obtenidos al aplicar algoritmos de mineria de datos',
+        }
+    )
 
