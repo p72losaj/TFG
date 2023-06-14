e5f3705c1dee3a6e758ac04e38c55b2e2a943b8b
Jaime Lorenzo Sanchez <jlorenzosa@gmail.com>
2023-06-14 11:30:08 +0200

    Finalizada estructura de la aplicacion web

--- TTFProyectDjango/TTFProyectDjango/TTFProyectDjango/settings.py
+++ TTFProyectDjango/TTFProyectDjango/TTFProyectDjango/settings.py
@@ -74,6 +74,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'TTFProyectDjango.wsgi.application'
 
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
