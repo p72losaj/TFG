725f19e1827c5b5011b05633ca5442c4bee879cb
Jaime Lorenzo Sanchez <jlorenzosa@gmail.com>
2023-06-14 11:30:08 +0200

    Finalizada estructura de la aplicacion web

--- TTFProyectDjango/TTFProyectDjango/TTFProyectDjango/urls.py
+++ TTFProyectDjango/TTFProyectDjango/TTFProyectDjango/urls.py
@@ -6,16 +6,13 @@ Definition of urls for TTFProyectDjango.
 from django.urls import path
 # from django.contrib import admin
 # from django.contrib.auth.views import LoginView, LogoutView
-from app import forms, views
+from app import views
 
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('tecnologias', views.tecnologias, name="tecnologias"),
-    
     path('MineriaDatos/', views.MineriaDatos, name='MineriaDatos'),
     path('simuladorPython', views.simuladorPython, name='simuladorPython'),
-    
-  
     
 ]
